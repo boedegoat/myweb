62d7cb345a2533f5bff02a0fb6e8a1de91094738
boedegoat <bhremada.fka@gmail.com>
2025-07-06 18:18:15 +0700

    revert start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
 docker compose up --build -d
-
-if [ $? -eq 0 ]; then
-  echo "Build and deploy successful. Cleaning up old images..."
-  docker image prune -f
-else
-  echo "Build or deploy failed. No cleanup performed."
-fi
